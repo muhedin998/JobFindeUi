54e47280410f53a79ca9133244fc78060a907776
Muhedin.Alic <muhedin1998@gmail.com>
2024-08-27 19:05:29 +0200

    changes from PC

--- src/app/config/auth.interceptors.ts
+++ src/app/config/auth.interceptors.ts
@@ -1,38 +1,44 @@
-import { Injectable } from "@angular/core";
+import {inject, Injectable} from '@angular/core';
 import {
-  HttpInterceptor,
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpErrorResponse,
-} from "@angular/common/http";
-import { catchError, Observable, throwError } from "rxjs";
-import { Router } from "@angular/router";
-import { Store } from "@ngrx/store";
-import { AppState } from "../core/store/state/app.state";
-import { RemoveUser } from "../core/store/actions/user.actions";
+    HttpInterceptor,
+    HttpRequest,
+    HttpHandler,
+    HttpEvent, HttpErrorResponse,
+} from '@angular/common/http';
+import {catchError, Observable, throwError} from 'rxjs';
+import {RemoveUser} from "../core/store/actions/user.actions";
+import {Store} from "@ngrx/store";
+import {AppState} from "../core/store/state/app.state";
 
 @Injectable()
 export class MyInterceptor implements HttpInterceptor {
-  constructor(private store: Store<AppState>) {}
-  intercept(
-    request: HttpRequest<any>,
-    next: HttpHandler,
-  ): Observable<HttpEvent<any>> {
-    const token = sessionStorage.getItem("token") ?? "";
-    const modifiedRequest = request.clone({
-      setHeaders: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    return next.handle(modifiedRequest).pipe(
-      catchError((error: HttpErrorResponse) => {
-        if (error.status === 401 || error.status === 403) {
-          sessionStorage.removeItem("token");
-          this.store.dispatch(new RemoveUser());
-        }
-        return throwError(error) as Observable<HttpEvent<any>>;
-      }),
-    );
-  }
+    private store = inject(Store<AppState>);
+    intercept(
+        request: HttpRequest<any>,
+        next: HttpHandler
+    ): Observable<HttpEvent<any>> {
+        // You can perform actions before the request is sent
+        console.log('Interceptor: Before sending request');
+        const token = sessionStorage.getItem('token') ?? '';
+        // Modify the request if needed
+        const modifiedRequest = request.clone({
+            setHeaders: {
+                'Authorization':`Bearer ${token}`,
+            }
+        });
+
+        // Continue with the modified request
+        return next.handle(modifiedRequest).pipe(
+            catchError((error: HttpErrorResponse) => {
+                // Check if the error is due to token expiration (status code 401)
+                if (error.status === 401 || error.status === 403) {
+                    console.log('Token expired, handle refresh logic here');
+                    sessionStorage.removeItem('token')
+                    sessionStorage.removeItem('user')
+                    this.store.dispatch(new RemoveUser())
+                }
+                return throwError(error);
+            })
+        );
+    }
 }
